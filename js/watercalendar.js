92314417256a35ffc60bc51a8a6fdfc94c171f44
Brandon <green.finite@gmail.com>
2017-11-13 11:16:48 -0800

    Added defaults to calendar

--- js/watercalendar.js
+++ js/watercalendar.js
@@ -1,3 +1,6 @@
+var reminderBtn = document.getElementById('setReminderForm');
+reminderBtn.addEventListener('submit', eatWaterReminderInput);
+
 function eatWaterReminderInput() {
   // Add the new plant reminder to the calendar
   var plantName =
@@ -5,31 +8,43 @@ function eatWaterReminderInput() {
   var frequency =
     document.getElementById('repeatFrequencySelector').value;
   var date = document.getElementById('dateModalEntry').value;
+  if (date == "") {
+    date = 1;
+  }
 
   // get every nth child of calendar
-  //var queryStr = 'ul.days > li:nth-child('+ frequency + 'n+' +
-  //date + ') > ul';
-  var queryStr = '.col-align-items-normal:nth-child(' + date +
-      ')';//' > ul';
-      var calendar = document.getElementById('calendar');
-      var children = calendar.querySelectorAll(':nth-child(' +
-          frequency + 'n+' + date + ')' + ' > ul');
+  var queryStr = '.col-align-items-normal:nth-child(' + date + ')'; //' > ul';
+  var calendar = document.getElementById('calendar');
+  var children = calendar.querySelectorAll(':nth-child(' + frequency + 'n+' + date + ')' + ' > ul');
 
-        for(var i = 0; i < children.length; i++) {
-        var newEle = document.createElement("li");
-        var eleText =
-        document.createTextNode(plantName);
-        newEle.appendChild(eleText);
-        children[i].appendChild(newEle);
-        }
+  // append the plant as a new li to each days ul list
+  for(var i = 0; i < children.length; i++) {
+    var newEle = document.createElement("li");
+    var eleText = document.createTextNode(plantName);
+    newEle.appendChild(eleText);
+    children[i].appendChild(newEle);
+  }
 
-        // append the plant as a new li to
-        // each days ul list
-        }
+}
 
-        function switchToDayView() {
-        document.location.href = "dayview.html/?";
-        var plants = document.getElementById('getme').value;
-        localStorage.setItem('plant', plants);
-        // fill in content on dayview page
-        }
+function switchToDayView() {
+  document.location.href = "dayview.html/?";
+  var plants = document.getElementById('getme').value;
+  localStorage.setItem('plant', plants);
+  // fill in content on dayview page
+}
+
+(function() {
+  'use strict';
+
+  window.addEventListener('load', function() {
+    var form = document.getElementById('setReminderForm');
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() == false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  }, false);
+})();
