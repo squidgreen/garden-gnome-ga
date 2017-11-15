a56f26b793717bc16ff911a56b8e89638e82fee7
Brian Vu <brian_vu2594@yahoo.com>
2017-11-14 23:06:17 -0800

    Added more functions

--- js/mygarden.js
+++ js/mygarden.js
@@ -1,6 +1,11 @@
+
+
+document.getElementById("savePlantButton").addEventListener('click', savePlant);
+
+
 //upload pic option for modal to add plant
 function uploadPic(){
-	var x = document.getElementById("myFile");
+	var x = document.getElementById("locationPic");
 	var txt = "";
 	if ('files' in x) {
 		if (x.files.length == 0) {
@@ -19,4 +24,23 @@ function uploadPic(){
 		}
 	} 
 	document.getElementById ("demo").innerHTML = txt;
+}
+
+function savePlant(){
+	var pName = document.getElementById('plantNameMyGardenModalEntry').value;
+
+	//check for duplicates. Duplicates not allowed
+	if (pName == list) {
+		alert ("That plant is already included in the list - please enter another plant");
+		return false;
+	}
+  
+	
+	var list = document.getElementById('pEntry');
+	var newLi = document.createElement('li');
+	var newPlantEntry = document.createTextNode(pName);
+	
+	newLi.appendChild(newPlantEntry);
+	list.appendChild(newLi);
+	
 }
