2a227e7899a420328eb6b2236adb7528ff14a817
Brian Vu <brian_vu2594@yahoo.com>
2017-11-14 17:49:39 -0800

    js for uploading pics in add plant modal

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,26 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//upload pic option for modal to add plant
+function uploadPic(){
+	var x = document.getElementById("myFile");
+	var txt = "";
+	if ('files' in x) {
+		if (x.files.length == 0) {
+			txt = "Select a file";
+		} else {
+			for (var i = 0; i < x.files.length; i++) {
+				txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+				var file = x.files[i];
+				if ('name' in file) {
+					txt += "name: " + file.name + "<br>";
+				}
+				if ('size' in file) {
+					txt += "size: " + file.size + " bytes <br>";
+				}
+			}
+		}
+	} 
+	document.getElementById ("demo").innerHTML = txt;
+}
